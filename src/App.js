801e631d78305d45a2786a3762892c887a40f953
BigBearDev <uzbigbear@yahoo.com>
2023-05-09 05:24:02 +0500

    build fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=(api_key_Openweathermap)`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=87db02bc77ece7d64825b7f2fd8fd6ec`
 
   const searchLocation = (event) => {
     if (event.key == 'Enter') {
